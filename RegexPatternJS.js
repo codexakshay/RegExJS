8af9eb1b5bf3b443e62ba786ff618c0dd5ba6241
AKSHAY <70620976+codexakshay@users.noreply.github.com>
2020-12-02 15:55:49 +0530

    Added : Regex pattern for zip code to validate

---
+++ RegexPatternJS.js
@@ -0,0 +1,11 @@
+const prompt = require('prompt-sync')();
+const ZIPCODE = RegExp("^[0-9]{6}$");
+let zip1 = "400088";// UC1 - Regex pattern to validate zip code 400088
+try{
+    if(!ZIPCODE.test(zip1)) throw "Invalid Zip code"
+    {
+        console.log("Valid Zip code");
+    }
+}catch(Exception){
+    console.log(Exception);
+}
